7bae408bc5cfe6cc09ed4b5d337ae8277baa3e95
radian 462 <kumita435675@gmail.com>
2025-07-21 04:03:07 +0000

    フォーマット

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,16 @@
-from bottle import route, run
 from threading import Thread
 
-@route('/')
+from bottle import route, run
+
+
+@route("/")
 def home():
     return "I'm alive"
 
+
 def run_server():
-    run(host='0.0.0.0', port=8080)
+    run(host="0.0.0.0", port=8080)
+
 
 def keep_alive():
     t = Thread(target=run_server)
