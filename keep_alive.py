8828981684ff3c3cfadb1d109840486ed2f0844d
radian 462 <kumita435675@gmail.com>
2025-07-21 03:59:26 +0000

    flaskをbottleに、pipをuvに

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,13 @@
-from flask import Flask
+from bottle import route, run
 from threading import Thread
 
-app = Flask('')
-
-@app.route('/')
+@route('/')
 def home():
     return "I'm alive"
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
+def run_server():
+    run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
+    t = Thread(target=run_server)
     t.start()
