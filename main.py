34bdd1e0fd42145cc3bad0b521a9b2f8287e1a2d
qiwi4673 <kooksannes@gmail.com>
2025-09-13 16:41:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import os
 from keep_alive import keep_alive
 
 client = discord.Client(intents=discord.Intents.default())
-intents = discord.Intents.default()
-intents.message_content = True
-bomb = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
