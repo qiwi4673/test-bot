5f619acac42409be03d0ef7d12110b71df4c7bdc
qiwi4673 <kooksannes@gmail.com>
2025-09-13 17:18:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import discord
 import os
 from keep_alive import keep_alive
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -10,8 +12,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    emoji ="🐌"
+    if message.author == client.user:
+        return
+
+    # 絵文字の追加
+    emoji = "🐌"
     await message.add_reaction(emoji)
+
+    # 特定のメッセージへの応答
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
