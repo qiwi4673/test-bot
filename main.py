989f2b0b8c698c762f54d06bf58fedbf489e6b17
qiwi4673 <kooksannes@gmail.com>
2025-09-13 16:18:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from keep_alive import keep_alive
 
 client = discord.Client(intents=discord.Intents.default())
 
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
 load_dotenv(verbose=True)
 
 @client.event
@@ -19,7 +24,18 @@ async def on_message(message):
     emoji ="🐌"
     await message.add_reaction(emoji)
 
+async def on_message(message):
+    # Bot自身のメッセージには応答しないようにします
+    if message.author == client.user:
+        return
 
+    # メッセージの内容が 'hello' だった場合
+    if message.content.startswith('やあ'):
+        # メッセージが送られたチャンネルに 'Hello!' と返信します
+        await message.channel.send('やあ')
+
+# 開発者ポータルで取得したボットトークンを設定
+# client.run('YOUR_BOT_TOKEN_HERE')
 
 
 
