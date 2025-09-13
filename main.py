d7cc9c738687b13fb6b44519e12397e73215d332
qiwi4673 <kooksannes@gmail.com>
2025-09-13 16:46:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from keep_alive import keep_alive
 
 client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -12,6 +15,17 @@ async def on_ready():
 async def on_message(message):
     emoji ="👍"
     await message.add_reaction(emoji)
+
+@client.event
+async def on_message(message):
+    # Bot自身のメッセージには応答しないようにします
+    if message.author == client.user:
+        return
+
+    # メッセージの内容が 'hello' だった場合
+    if message.content.startswith('hello'):
+        # メッセージが送られたチャンネルに 'Hello!' と返信します
+        await message.channel.send('Hello!')
 
 keep_alive()
 
