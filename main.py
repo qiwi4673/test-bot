5cd7c9e8ba7d7e783ea391656639e232daad9c36
qiwi4673 <kooksannes@gmail.com>
2025-09-14 22:11:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def on_message(message):
         ]
         # リストからランダムに1つ選んで送信
         await message.channel.send(random.choice(responses))
-        
+    
     elif message.content == 'たけ':
         # 1から6までの整数をランダムに生成
         dice_result = random.randint(1, 100)
@@ -62,6 +62,23 @@ async def on_message(message):
         custom_emoji = client.get_emoji(1415213398546714704)
         if custom_emoji:
             await message.add_reaction(custom_emoji)
+ if message.content.startswith('んろう、こんだて'):
+        # 1つ目のランダムな要素のリスト
+        subject = ['そば', 'フカヒレスープ', 'パフェ', '小籠包', 'レモン', 'アイスクリーム', 'シュウマイ', 'スープ', '寿司', '春巻き', 'ワッフル', 'ローストチキン', 'シリアル', 'パスタ', 'カツ丼', 'カヌレ', 'フレンチトースト', 'オレンジジュース', '酢豚', '餃子', 'プリン', 'もつ鍋', 'クリームチーズ', 'パンケーキ', 'ナッツ', 'カレーライス', 'ホットドッグ', 'ショートケーキ', 'フォンダンショコラ', 'スパゲッティ', 'クロワッサン', 'サンドイッチ', 'ミルク', '親子丼', '紅茶', 'しゃぶしゃぶ', 'エビフライ', 'トマト鍋', 'ハンバーガー', 'ドーナツ', 'ピザ', '麻婆豆腐', '北京ダック', '牛丼', 'たこ焼き', 'うどん', 'マカロン', 'パンナコッタ', 'フライドポテト', '天ぷら', '味噌汁', '回鍋肉', 'キムチ鍋', 'クッキー', 'ラーメン', 'クレープ', '焼き肉', 'エビチリ', 'コーヒー', 'マフィン']
+        # 2つ目のランダムな要素のリスト
+        action = [
+    "を焼いたもの", "を煮たもの", "を炒めたもの", "を揚げたもの", "を蒸したもの",
+    "茹でたもの", "を炙ったもの", "を和えたやつ", "を漬けたもの", "を燻したもの",
+    "を蒸し焼きにしたもの", "の炒め煮", "を炊いたもの", "の網焼き", "の串焼き",
+    "のソテー", "のグリル", "をローストしたもの", "のポシェ", "をフランベしたもの"
+]]
+        
+        # 各リストからランダムに1つずつ選択
+        random_subject = random.choice(subject)
+        random_action = random.choice(action)
+        
+        # 2つの要素を組み合わせて返信
+        await message.channel.send(f'はい \n {random_subject}{random_action}。')
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
 keep_alive()
