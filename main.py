b57f27d75d65550ba3e71efd53ce01de21290bb0
qiwi4673 <kooksannes@gmail.com>
2025-09-16 21:22:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print('ログインしました')
     
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="世界"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.competing, name="カタツムリと"))
 
 @client.event
 async def on_message(message):
