e39934d053ca703437651562d3aba38f7f468a0a
qiwi4673 <kooksannes@gmail.com>
2025-09-16 19:12:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ async def on_message(message):
     # メッセージの末尾が「かな？」で終わるかチェック
     if message.content.strip().endswith('かな？'):
         questionagree = ['うん、','えっ','う〜ん']
-        questionletter = ['違うかも〜','そうかも〜','違うよ〜','それだよ〜']
+        questionletter = ['違うかも〜','そうかも〜','違うよ〜','そうだよ〜']
         random_questionagree = random.choice(questionagree)
         random_questionletter = random.choice(questionletter)
         await message.channel.send(f'{random_questionagree}{random_questionletter}。')
