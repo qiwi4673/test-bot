fd7b66d70f98bb2aca6df6d80223f4e15e9f0937
qiwi4673 <kooksannes@gmail.com>
2025-09-18 18:42:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ async def on_message(message):
     if message.content.startswith('ぼれろ〜'):
         # メッセージが「ぼれろ〜」のみの場合
         if len(message.content.strip()) == 4:
-            commonreply = ['どうしました〜？','なぁに〜？','ん〜？']
+            commonreply = ['どうしました〜？','なぁに〜？','ん〜？','とってもすごいBoleroです！']
             random_commonreply = random.choice(commonreply)
             await message.channel.send(f'{random_commonreply}')
         # 5文字以上の場合
